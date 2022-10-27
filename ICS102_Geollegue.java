049915e4d0db4b24d609aac8b48a36322450f6a5
Hanzera <114341735+hanzgeollegue@users.noreply.github.com>
2022-10-27 16:56:02 +0800

    Add files via upload

---
+++ ICS102_Geollegue.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ics102_geollegue;
+
+/**
+ *
+ * @author GEOLLEGUE-ICS102
+ */
+public class ICS102_Geollegue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
